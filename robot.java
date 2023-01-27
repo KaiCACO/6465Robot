51dbb5ae0be279e4b99eb63bfb0ff498fd9af5d6
KaiCACO <115180903+KaiCACO@users.noreply.github.com>
2023-01-27 15:36:53 -0800

    Update robot.java

--- robot.java
+++ robot.java
@@ -9,23 +9,18 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-//import edu.wpi.first.util.net.PortForwarder;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-// import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.GenericHID.RumbleType;
-//import org.photonvision.PhotonCamera;
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -33,11 +28,11 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
-
 public class Robot extends TimedRobot {
-  // private static final String kDefaultAuto = "Shoot and Drive";
-  // private static final String kCustomAuto = "Shoot";
-  // private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  private static final String kDefaultAuto = "Shoot and Drive";
+  private static final String kCustomAuto = "Shoot";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
   
   NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
   NetworkTableEntry tx = table.getEntry("tx");
@@ -50,37 +45,42 @@ public class Robot extends TimedRobot {
   Double dist = null;
   Double xd1;
 
-  public final Compressor compressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
-  private final XboxController m_Xbox_Drive = new XboxController(1);
-  private final Joystick m_Joystick_Drive = new Joystick(0);
+  private final Joystick m_Xbox_Drive = new Joystick(0);
+  private final XboxController m_Xbox_Co_Drive = new XboxController(1);
 
-  private final CANSparkMax m_Front_Left = new CANSparkMax(2, MotorType.kBrushless);
-  private final CANSparkMax m_Front_Right = new CANSparkMax(3, MotorType.kBrushless);
-  private final CANSparkMax m_Back_Left = new CANSparkMax(1, MotorType.kBrushless);
-  private final CANSparkMax m_Back_Right = new CANSparkMax(9, MotorType.kBrushless);
+  private final Compressor pcm_Compressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
 
-  private final CANSparkMax m_elevator = new CANSparkMax(10, MotorType.kBrushless);
-  private final CANSparkMax m_intake = new CANSparkMax(8, MotorType.kBrushless);
-
-  private final DigitalInput elevator_top_switch = new DigitalInput(9);
+  private final CANSparkMax m_Front_Left = new CANSparkMax(9, MotorType.kBrushless);
+  private final CANSparkMax m_Front_Right = new CANSparkMax(2, MotorType.kBrushless);
+  private final CANSparkMax m_Back_Left = new CANSparkMax(3, MotorType.kBrushless);
+  private final CANSparkMax m_Back_Right = new CANSparkMax(1, MotorType.kBrushless);
 
   private final MotorControllerGroup m_Left = new MotorControllerGroup(m_Front_Left, m_Back_Left);
   private final MotorControllerGroup m_Right = new MotorControllerGroup(m_Front_Right, m_Back_Right);
 
-  private final DifferentialDrive m_Drive = new DifferentialDrive(m_Left, m_Right); 
-  
-  private final DoubleSolenoid solenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 4, 3);
+  private final DifferentialDrive m_Drive = new DifferentialDrive(m_Left, m_Right);
+
+  private final DoubleSolenoid p_Intake_Left = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 3);
+  private final DoubleSolenoid p_Intake_Right = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 0);
+
+  //private final CANSparkMax m_Aim = new CANSparkMax(8, MotorType.kBrushless);
+  private final CANSparkMax m_Intake = new CANSparkMax(10, MotorType.kBrushed);
+  private final CANSparkMax m_Elevator = new CANSparkMax(6, MotorType.kBrushless);
+  private final CANSparkMax m_Winch = new CANSparkMax(5, MotorType.kBrushless);
+  private final CANSparkMax m_Shoot1 = new CANSparkMax(4, MotorType.kBrushed);
+  private final CANSparkMax m_Shoot2 = new CANSparkMax(7, MotorType.kBrushed);
+  private final CANSparkMax m_Shoot_Back = new CANSparkMax(8, MotorType.kBrushed);
 
   private final Timer m_timer = new Timer();
+  private final Timer m_timer2 = new Timer();
+  public static boolean m_first = false;
+  public static boolean m_second = false;
+  public static boolean m_third = false;                                                                                                                                                                                                                                                                                                                                                                                                                              
+  private boolean m_stopAndShoot = false;
+  private boolean m_driveer = false;
+  private boolean m_done = false;
+  private double speed = 0;
   private double getY;
-  private double getX;
-  private double speed;
-  private boolean held = false;
-  private final Timer intake_ease_timer = new Timer();
-
-  private int whichAuto = 2;
-  //PhotonCamera camera = new PhotonCamera("photonvision");
-  //PortForwarder.add(5800, "photonvision.local", 5800);
 
 
 
@@ -90,49 +90,40 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    compressor.enableDigital();
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
 
-    // m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    // m_chooser.addOption("My Auto", kCustomADasuto);
-    // SmartDashboard.putData("Auto choices", m_chooser);
-    if (m_Xbox_Drive.getLeftBumper()) { 
-      solenoid.set(DoubleSolenoid.Value.kForward);
-
-    } else if (m_Xbox_Drive.getRightBumper()) { 
-      solenoid.set(DoubleSolenoid.Value.kReverse);
-
-    } 
 
     m_Left.setInverted(false);
     m_Right.setInverted(true);
-    
+
+
   }
-
-
-
-  
 
   /**
    * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
    * that you want ran during disabled, autonomous, teleoperated and test.
    *
+   * 
+   * 
    * <p>This runs after the mode specific periodic functions, but before LiveWindow and
    * SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
-    // double x = tx.getDouble(0.0);
-    // double y = ty.getDouble(0.0);
-    // double area = ta.getDouble(0.0);
+    double x = tx.getDouble(0.0);
+    double y = ty.getDouble(0.0);
+    double area = ta.getDouble(0.0);
 
-    // ang = (y * (Math.PI / 180)) + 0.4188;
+    ang = (y * (Math.PI / 180)) + 0.4188;
 
-    // dist =  64 / Math.tan(ang);
+    dist =  64 / Math.tan(ang);
 
-    // SmartDashboard.putNumber("Distance", dist);
-    // SmartDashboard.putNumber("LimelightX", x);
-    // SmartDashboard.putNumber("LimelightY", y);
-    // SmartDashboard.putNumber("LimelightArea", area);
+    SmartDashboard.putNumber("Distance", dist);
+    SmartDashboard.putNumber("LimelightX", x);
+    SmartDashboard.putNumber("LimelightY", y);
+    SmartDashboard.putNumber("LimelightArea", area);
   }
 
   /**
@@ -147,113 +138,255 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
-    System.out.println("starting autonomous");
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+
+    m_Elevator.set(0);
+    m_Shoot1.set(0);
+    m_Shoot2.set(0);
+    m_Shoot_Back.set(0);
+
     m_timer.reset();
     m_timer.start();
+    m_stopAndShoot = false;
+    m_driveer = false;
+    m_done = false;
   }
 
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    
-    
-    if (whichAuto == 1) {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        if(m_stopAndShoot == false){
+          // m_Left.set(.5);
+          // m_Right.set(.5);
+          m_Shoot1.set(0.48);
+          m_Shoot2.set(-0.48);
+        }
+        if(m_timer.advanceIfElapsed(5.0)){
+          m_Elevator.set(0.40);
+          m_stopAndShoot = true;
+        }
+        break;
+      case kDefaultAuto:
+      default:
 
-      // goes 28 inches in 1.0 second at 0.20 power
-      if (m_timer.get() < 1.0) {
-        m_Left.set(.20);
-        m_Right.set(.20);
+      if(m_stopAndShoot == false){
+        m_Shoot1.set(0.48);
+        m_Shoot2.set(-0.48); 
+        if(m_timer.advanceIfElapsed(5.0)){
+          m_Elevator.set(0.40);
+          m_stopAndShoot = true;
+        }
       }
-      // rotates ~90 degrees in 0.9 seconds at 0.20 power
-      else if ((m_timer.get() > 1.2) && (m_timer.get() < 2.1)) {
-        m_Left.set(0.20);
-        m_Right.set(-0.20);
+
+      if(m_stopAndShoot == true && m_driveer == false){
+        if(m_timer.advanceIfElapsed(1.0)){
+          m_Elevator.set(0);
+          m_Shoot1.set(0);
+          m_Shoot2.set(0);
+          m_driveer = true;
+        }
       }
-      else if ((m_timer.get() > 2.3) && (m_timer.get() < 2.8)) {
-        m_Left.set(.20);
-        m_Right.set(.20);
+
+      if(m_driveer == true && m_done == false){
+          m_Left.set(.20);
+          m_Right.set(.20);
+          if(m_timer.advanceIfElapsed(3.5)){
+            m_done = true;
+          }
       }
-      else {
-        m_Left.set(0);
-        m_Right.set(0);
+
+      if(m_done == true){
+        m_Left.set(0.0);
+        m_Right.set(0.0);
       }
+
+        break;
     }
-
-    else if (whichAuto == 2) {
-      System.out.println(m_timer.get());
-      if (m_timer.get() < 11) {
-        m_Left.set(.7);
-        m_Right.set(.7);
-      }
-      else {
-        m_Left.set(0);
-        m_Right.set(0);
-      }
-
-      if (elevator_top_switch.get() && m_timer.get() < 9) {
-        m_elevator.set(.20);
-      }
-      else {
-        m_elevator.set(0);
-      }
-    }
-
   }
 
   /** This function is called once when teleop is enabled. */
   @Override
   public void teleopInit() {
-    System.out.println("TeleopInit");
+    p_Intake_Left.set(Value.kForward);
+    p_Intake_Right.set(Value.kForward);
+    pcm_Compressor.enableDigital();
   }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    getY = -m_Joystick_Drive.getY();
-    getX = m_Joystick_Drive.getX();
 
-    speed = speed + (-speed)/20;
+    getY = -m_Xbox_Drive.getY();
+
+    speed = speed + (getY - speed)/12;
+
+    m_Drive.arcadeDrive(speed/1.4, m_Xbox_Drive.getX());
+
+    if(m_Xbox_Co_Drive.getLeftTriggerAxis() > .50){
+      m_Intake.set(m_Xbox_Co_Drive.getLeftTriggerAxis());
+    }
+    else{
+      m_Intake.set(0.0);
+    }
+
     
-    m_Drive.arcadeDrive(speed/1.0, getX/1.4);
 
-    if (m_Xbox_Drive.getLeftBumper() || m_Xbox_Drive.getRightBumper()) {
-      if (held == false) {
-        held = true;
-        intake_ease_timer.reset();
-        intake_ease_timer.start();
+    // if(m_Xbox_Co_Drive.getRightTriggerAxis() > .10){
+    //   double y1 = dist;
+    //   double m = 3.73289;
+    //   double b = -101.579;
+    //   xd1 = (.01 * ((y1 - b) / m)) + 0.18;
+
+    //   if(xd1 > 1.00){
+    //     xd1 = 1.00;
+    //   }
+    //   if(xd1 < 0.25){
+    //     xd1 = 0.00;
+    //   }
+    //   m_Shoot1.set(xd1);
+    //   m_Shoot2.set(-xd1);
+    // }
+    if(m_Xbox_Co_Drive.getYButton() == true){
+
+      if(m_first == false && m_second == false && m_third == false){
+          m_Shoot1.set(0.48);
+          m_Shoot2.set(-0.48);
+
+          m_timer2.start();
+          if(m_timer2.advanceIfElapsed(1.3)){
+              m_Elevator.set(.40);
+              m_first = true;
+          }
       }
-      if (m_Xbox_Drive.getLeftBumper()) {
-        if (intake_ease_timer.get() > 0.5) {
-          m_intake.set(0.6);
-        }
-        else {
-          m_intake.set(0.6*(intake_ease_timer.get()*2));
-        }
-        m_Xbox_Drive.setRumble(RumbleType.kLeftRumble, 0.5);
-        m_Xbox_Drive.setRumble(RumbleType.kRightRumble, 0);
+
+      if(m_first == true && m_second == false && m_third == false){
+          if(m_timer2.advanceIfElapsed(0.2)){
+              m_Elevator.set(0.0);
+              m_second = true;
+          }
       }
-      else if (m_Xbox_Drive.getRightBumper()) {
-        m_intake.set(-0.6);
-        m_Xbox_Drive.setRumble(RumbleType.kLeftRumble, 0);
-        m_Xbox_Drive.setRumble(RumbleType.kRightRumble, 0.5);
+      if(m_first == true && m_second == true && m_third == false){
+          if(m_timer2.advanceIfElapsed(1.2)){
+              m_Elevator.set(0.40);
+              m_third = true;
+          }
       }
-    }
-    else {
-      m_intake.set(0);
-      m_Xbox_Drive.setRumble(RumbleType.kLeftRumble, 0);
-      m_Xbox_Drive.setRumble(RumbleType.kRightRumble, 0);
-      held = false;
+      if(m_first == true && m_second == true && m_third == true){
+          if(m_timer2.advanceIfElapsed(1.0)){
+              m_Elevator.set(0.0);
+              m_Shoot1.set(0);
+              m_Shoot2.set(0);
+
+              m_timer2.stop();
+              m_timer2.reset();
+
+              m_first = false;
+              m_second = false;
+              m_third = false;
+          }
+      }
+
+  }
+    else{
+
+      if(m_Xbox_Co_Drive.getRightY() > 0.20){
+        m_Elevator.set(0.40);
+      }
+      else if (m_Xbox_Co_Drive.getRightY() < -0.20 ){
+        m_Elevator.set(-0.40);
+      }
+      else{
+        m_Elevator.set(0.00);
+      }    
+
+      if(m_Xbox_Co_Drive.getBButton() == true){
+        m_Shoot1.set(0.7);
+        m_Shoot2.set(-0.7);
+  
+      }
+
+      else {
+        m_Shoot1.set(0.00);
+        m_Shoot2.set(0.00);
+      }
+
+      if(m_Xbox_Co_Drive.getXButton()){
+        m_Shoot_Back.set(0.5);
+      }
+
+      else if(m_Xbox_Co_Drive.getAButton()){
+        m_Shoot_Back.set(-0.5);
+      }
+
+      else {
+        m_Shoot_Back.set(0);
+      }
+
+
+
     }
 
-    if (m_Xbox_Drive.getRightTriggerAxis() > 0.08 && elevator_top_switch.get()) {
-      //elevatur go upsies
-      m_elevator.set(0.7*(m_Xbox_Drive.getRightTriggerAxis()));
+
+
+
+    if(m_Xbox_Co_Drive.getStartButtonPressed()){
+      m_Winch.set(.75);
     }
-    else if (m_Xbox_Drive.getLeftTriggerAxis() > 0.08) {
-      //elevatur go downsies
-      m_elevator.set(-0.7*(m_Xbox_Drive.getLeftTriggerAxis()));
+    else if (m_Xbox_Co_Drive.getStartButton() == false){
+      if(m_Xbox_Co_Drive.getBackButton() == true){
+        m_Winch.set(-0.75);
+      }
+      else if(m_Xbox_Co_Drive.getStartButton() == false){
+        m_Winch.set(0.00);
+      }
     }
-    else {m_elevator.set(0);}
+
+
+    // if(m_Xbox_Co_Drive.getAButtonPressed()){
+
+    //   double x = tx.getDouble(0.0);
+
+    //   if(x > .2){
+    //     m_Aim.set(.2);
+    //   }
+    //   if(x < -.12){
+    //     m_Aim.set(-.2);
+    //   }
+    //   if (x > -.2 && x < .2) {
+    //     m_Aim.set(0.0);
+    //   }
+    // }
+    // else if (m_Xbox_Co_Drive.getLeftX() > 0.20 || m_Xbox_Co_Drive.getLeftX() < -0.20){
+    //   m_Aim.set(m_Xbox_Co_Drive.getLeftX());
+    // }
+    // // else if (m_Xbox_Co_Drive.getLeftX() > 0.00 && m_Xbox_Co_Drive.getLeftX() < 0.00){
+    // //   m_Aim.set(0.00);
+    // // }
+    // else{
+    //   m_Aim.set(0.00);
+    // }
+
+    
+      
+    
+    if(m_Xbox_Co_Drive.getLeftBumperPressed()){
+      p_Intake_Left.set(Value.kReverse);
+      p_Intake_Right.set(Value.kReverse);
+      //r extend
+    }
+    if(m_Xbox_Co_Drive.getRightBumperPressed()){
+      p_Intake_Left.set(Value.kForward);
+      p_Intake_Right.set(Value.kForward);
+      //r retract
+    }
+    else if (m_Xbox_Drive.getRawButtonPressed(2)){
+      p_Intake_Left.set(Value.kForward);
+      p_Intake_Right.set(Value.kForward);
+    }
   }
 
   /** This function is called once when the robot is disabled. */
@@ -274,6 +407,19 @@ public class Robot extends TimedRobot {
   @Override
   public void testPeriodic() {
 
+        pcm_Compressor.enableDigital();
+
+    if(m_Xbox_Co_Drive.getStartButtonPressed()){
+      m_Winch.set(.50);
+    }
+    else if (m_Xbox_Co_Drive.getStartButton() == false){
+      if(m_Xbox_Co_Drive.getBackButton() == true){
+        m_Winch.set(-0.40);
+      }
+      else if(m_Xbox_Co_Drive.getStartButton() == false){
+        m_Winch.set(0.00);
+      }
+    }
   }
 
   /** This function is called once when the robot is first started up. */
