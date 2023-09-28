adbc0a5f990c8fc8ebcba2240397848efe95e7ae
KaiCACO <kaisereni@cascadesacademy.org>
2023-09-27 17:49:29 -0700

    update

--- 2024-robot/src/main/java/frc/robot/robotHighLevel.java
+++ 2024-robot/src/main/java/frc/robot/robotHighLevel.java
@@ -19,9 +19,27 @@ public class robotHighLevel {
   private static Joystick m_Joystick = new Joystick(OIConstants.joystickPort);
 
   public static void updateValues() {
-    transX = m_Joystick.getX();
-    transY = -m_Joystick.getY();
+    // joystickVector returns list of [controllerAmplitude, controllerAngle]
+    var joystickVector = vectorize(m_Joystick.getX(), -m_Joystick.getY());
+    // robotRot contains robot rotation from gyro
+    var robotRot = 0.0;
+    joystickVector[1] = joystickVector[1] - robotRot;
+    var amp = joystickVector[0];
+    var ang = joystickVector[1];
+
+    // now amp and ang contain robot movement speed and angle
+    transX = Math.sqrt((amp*amp) + (Math.sin(ang)*Math.sin(ang)));
+    transY = Math.sqrt((amp*amp) + (Math.cos(ang)*Math.cos(ang)));
     rot = m_Xbox.getRightX();
+  }
+
+  private static double[] vectorize(double x, double y) {
+    var amp = Math.sqrt((x*x)+(y*y));
+    var theta = Math.atan(y/x);
+    double[] ret = new double[2];
+    ret[0] = amp;
+    ret[1] = theta;
+    return ret;
   }
 
 }
