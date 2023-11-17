445b5b99d465784e3896145f19d63d2ba756f293
KaiCACO <kaisereni@cascadesacademy.org>
2023-11-16 17:39:27 -0800

    update

--- 2024-robot/src/main/java/frc/robot/Robot.java
+++ 2024-robot/src/main/java/frc/robot/Robot.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj.XboxController;
@@ -26,74 +27,97 @@ public class Robot extends TimedRobot {
 
   private XboxController xbox = new XboxController(1);
   private Joystick joystick = new Joystick(0);
-
   private DigitalInput limitSwitch = new DigitalInput(1);
-
   private Command m_autonomousCommand;
-
   private RobotContainer m_robotContainer;
-
   private static WPI_Pigeon2 gyro = new WPI_Pigeon2(12);
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  private Timer autoTimer = new Timer();
+  private boolean onRamp = false;
+
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
   }
-
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
+  
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
   }
 
-  /** This function is called once each time the robot enters Disabled mode. */
   @Override
   public void disabledInit() {}
 
   @Override
   public void disabledPeriodic() {}
 
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    gyro.setYaw(0.0);
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
-  }
-
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void teleopInit() {
+    gyro.reset();
+    gyro.configFactoryDefault();
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
-    gyro.setYaw(0.0);
+    autoTimer.stop();
+    autoTimer.reset();
+    autoTimer.start();
+    RobotContainer.rot = 0.0;
+    RobotContainer.transX = 0.0;
+    RobotContainer.transY = 0.0;
   }
 
-  /** This function is called periodically during operator control. */
+  @Override
+  public void autonomousPeriodic() {
+    var angle = -gyro.getRoll();
+    if (onRamp == false && angle > 5) {
+      onRamp = true;
+      autoTimer.stop();
+      autoTimer.reset();
+      autoTimer.start();
+    }
+    if(true) {
+      if (autoTimer.get() < 3.3 && onRamp == false) {
+        RobotContainer.transX = 0.2;
+      }
+      else if (onRamp == true) {
+        RobotContainer.transX = angle/(20+((autoTimer.get()*1.5)));
+      }
+      else {
+        RobotContainer.transX = 0.0;
+      }
+    }
+  }
+
+  @Override
+  public void teleopInit() {
+    gyro.reset();
+    gyro.configFactoryDefault();
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
   @Override
   public void teleopPeriodic() {
     // var xboxVector = positionToVector(xbox.getLeftX(), xbox.getLeftY());
     // RobotContainer.rot = xboxVector[1] * calculateRotationSpeed(gyro.getYaw(), xboxVector[0]);
-    RobotContainer.rot = xbox.getLeftX();
+    RobotContainer.rot = joystick.getZ()*joystick.getZ()/1.5;
+    if (joystick.getZ() < 0) {
+      RobotContainer.rot = -RobotContainer.rot;
+    }
     RobotContainer.rotOffset = -gyro.getYaw();
 
-    var newValues = offsetJoystick(joystick.getX(), -joystick.getY(), RobotContainer.rotOffset-90);
+    var x = joystick.getX();
+    var y = joystick.getY();
+
+    if (Math.abs(x) < 0.03) {
+      x = 0;
+    }
+    if (Math.abs(y) < 0.03) {
+      y = 0;
+    }
+
+    var newValues = offsetJoystick(x, -y, RobotContainer.rotOffset-90);
     RobotContainer.transX = newValues[0];
     RobotContainer.transY = newValues[1];
     
@@ -124,17 +148,12 @@ public class Robot extends TimedRobot {
 
   @Override
   public void testInit() {
-    // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
-    gyro.setYaw(0.0);
   }
 
-  /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {
-    var xboxDir = positionToVector(xbox.getRightX(), -xbox.getRightY());
-    System.out.println("direction: " + xboxDir[0]);
-    System.out.println("amp: " + xboxDir[1]);
+    System.out.println("yaw: " + gyro.getYaw());
   }
 
   private static double[] offsetJoystick(double x, double y, double degrees) {
@@ -145,25 +164,6 @@ public class Robot extends TimedRobot {
 
     double[] newValues = {newX, newY};
     return newValues;
-  }
-
-  private static double[] positionToVector(double x, double y) {
-    var angle = Math.toDegrees(Math.atan2(y, x));
-    var dis = Math.sqrt((x*x) + (y*y))/1.125;
-    return new double[]{angle, dis};
-  }
-
-  private static double calculateRotationSpeed(double currentRotation, double desiredRotation) {
-    var rotationDifference = desiredRotation - currentRotation;
-
-    if(rotationDifference > 180) {
-      rotationDifference -= 360;
-    }
-    else if(rotationDifference < -180) {
-      rotationDifference += 360;
-    }
-
-    return rotationDifference/180;
   }
 
 }
